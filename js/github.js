98b85a816e122418b0313ca0c31b7a15c4d0e699
DeveloperLimitless <50052902+DeveloperLimitless@users.noreply.github.com>
2026-02-28 00:21:15 +0800

    Add files via upload

--- js/github.js
+++ js/github.js
@@ -1,13 +1,5 @@
-function open_card() {
-  document.getElementById('front').className = 'open-card';
-}
+const card = document.getElementById("card");
 
-function close_card() {
-  document.getElementById('front').className = '';
-}
-
-var button=document.getElementById("front");
-button.addEventListener("click", open_card, false);
-
-var button=document.getElementById("inside");
-button.addEventListener("click", close_card, false);
+card.addEventListener("click", () => {
+    card.classList.toggle("open");
+});
